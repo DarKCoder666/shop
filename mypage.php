21619f678fb2d203522199e5b54f05609aa474a5
DarKCoder666 <umid010699@mail.ru>
2017-11-27 12:27:43 +0500

    Настроены отображение товаров по категориям на главной странице

--- mypage.php
+++ mypage.php
@@ -3,42 +3,24 @@
 
 
 get_header(); 
-
-
 ?>
-
 
 
 <!--Главные категории-->
 <div class="wrap_block">
 	<div class="top_category">
 		<ul class="ul_cat">
-			<li><img src="<?php bloginfo('template_directory'); ?>/images/shirts.png"><a href="<?php echo get_term_link( 15 ,'product_cat') ?>">Текстиль и одежда<span>&#9660;</span></a>
-				<?php woocommerce_subcats_from_parentcat_by_ID(15); ?>
+			<li><img src="<?php bloginfo('template_directory'); ?>/images/shirts.png"><a href="<?php echo get_term_link( 26 ,'product_cat') ?>">Текстиль и одежда<span>&#9660;</span></a>
+				<?php woocommerce_subcats_from_parentcat_by_ID(26); ?>
 			</li>
-			<li><img src="<?php bloginfo('template_directory'); ?>/images/ham.png"><a href="">Фасфуд и напитки<span>&#9660;</span></a>
-				<ul>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-				</ul>
+			<li><img src="<?php bloginfo('template_directory'); ?>/images/ham.png"><a href="<?php echo get_term_link( 29 ,'product_cat') ?>">Фасфуд и напитки<span>&#9660;</span></a>
+				<?php woocommerce_subcats_from_parentcat_by_ID(29); ?>
 			</li>
-			<li><img src="<?php bloginfo('template_directory'); ?>/images/cake.png"><a href="">Сладости и кофе<span>&#9660;</span></a>
-				<ul>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-				</ul>
+			<li><img src="<?php bloginfo('template_directory'); ?>/images/cake.png"><a href="<?php echo get_term_link( 27 ,'product_cat') ?>">Сладости и кофе<span>&#9660;</span></a>
+				<?php woocommerce_subcats_from_parentcat_by_ID(27); ?>
 			</li>
-			<li><img src="<?php bloginfo('template_directory'); ?>/images/market.png"><a href="">Маркет<span>&#9660;</span></a>
-				<ul>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-					<li><a href="">Пункт меню 1</a></li>
-				</ul>
+			<li><img src="<?php bloginfo('template_directory'); ?>/images/market.png"><a href="<?php echo get_term_link( 28 ,'product_cat') ?>">Маркет<span>&#9660;</span></a>
+				<?php woocommerce_subcats_from_parentcat_by_ID(28); ?>
 			</li>
 		</ul>
 		<div class="clear"></div>
@@ -218,19 +200,182 @@ get_header();
 <div class="sales_block background_none">
 	<ul class="ul_cat">
 		<li><img src="<?php bloginfo('template_directory'); ?>/images/shirts.png"><a href="">Текстиль и одежда<span>&#9660;</span></a>
-			<ul>
-				<li><a href="">Пункт меню 1</a></li>
-				<li><a href="">Пункт меню 1</a></li>
-				<li><a href="">Пункт меню 1</a></li>
-				<li><a href="">Пункт меню 1</a></li>
-			</ul>
+			<?php woocommerce_subcats_from_parentcat_by_ID(26); ?>
 		</li>
 	</ul>
 	<div class="clear"></div>
 	<?php
 	$args = array(
 		'post_type' => 'product',
-		'product_cat' => 'accessories',
+		'product_cat' => 'tekstil',
+		'posts_per_page' => 4
+	);
+
+	$cont_num =0;
+	$loop = new WP_Query( $args );
+	if ( $loop->have_posts() ) {
+		while ( $loop->have_posts() ) : $loop->the_post();
+			++$cont_num;
+			?>
+			<div class="product_item">
+				<div class="border_right <?php if($cont_num==4) echo 'border_none'; ?>">
+					<a href="<?php the_permalink() ?>" class="img_product">
+						<?php echo woocommerce_get_product_thumbnail(); ?>
+					</a>
+					<a href="<?php the_permalink() ?>" title="Ссылка на: <?php the_title_attribute(); ?>" class="title_profuct"><?php the_title(); ?></a>
+					<div class="new_price"><?php echo $product->price; ?> сум</div>
+
+					<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
+					<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+					<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+
+					<?php
+					if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
+						$html = '<form  class="cart" method="post" product_id="'. $product->get_id() .  '" enctype="multipart/form-data">';
+						$html .= '<div class="num_select">'.woocommerce_quantity_input( array(), $product, false ).'</div>';
+						$html .= '<div class="but_add"><button type="submit">добавить</button></div>';
+						$html .= '</form>';
+						echo $html;
+					} elseif ( $product->is_type( 'variable' ) ) {
+					?>
+						<div class="but_add"><a href="<?php the_permalink() ?>">выбрать</a></div>
+					<?php } ?>
+					<div class="clear"></div>
+				</div>
+			</div>
+		<?php endwhile;
+	} else {
+		echo __( 'No products found' );
+	}
+	wp_reset_postdata();
+	?>
+	<div class="clear"></div>
+
+</div>
+
+<div class="sales_block background_none">
+	<ul class="ul_cat">
+		<li><img src="<?php bloginfo('template_directory'); ?>/images/ham.png"><a href="">Фастфуд и напитки<span>&#9660;</span></a>
+			<?php woocommerce_subcats_from_parentcat_by_ID(29); ?>
+		</li>
+	</ul>
+	<div class="clear"></div>
+	<?php
+	$args = array(
+		'post_type' => 'product',
+		'product_cat' => 'fastfood',
+		'posts_per_page' => 4
+	);
+
+	$cont_num =0;
+	$loop = new WP_Query( $args );
+	if ( $loop->have_posts() ) {
+		while ( $loop->have_posts() ) : $loop->the_post();
+			++$cont_num;
+			?>
+			<div class="product_item">
+				<div class="border_right <?php if($cont_num==4) echo 'border_none'; ?>">
+					<a href="<?php the_permalink() ?>" class="img_product">
+						<?php echo woocommerce_get_product_thumbnail(); ?>
+					</a>
+					<a href="<?php the_permalink() ?>" title="Ссылка на: <?php the_title_attribute(); ?>" class="title_profuct"><?php the_title(); ?></a>
+					<div class="new_price"><?php echo $product->price; ?> сум</div>
+
+					<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
+					<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+					<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+
+					<?php
+					if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
+						$html = '<form  class="cart" method="post" product_id="'. $product->get_id() .  '" enctype="multipart/form-data">';
+						$html .= '<div class="num_select">'.woocommerce_quantity_input( array(), $product, false ).'</div>';
+						$html .= '<div class="but_add"><button type="submit">добавить</button></div>';
+						$html .= '</form>';
+						echo $html;
+					} elseif ( $product->is_type( 'variable' ) ) {
+					?>
+						<div class="but_add"><a href="<?php the_permalink() ?>">выбрать</a></div>
+					<?php } ?>
+					<div class="clear"></div>
+				</div>
+			</div>
+		<?php endwhile;
+	} else {
+		echo __( 'No products found' );
+	}
+	wp_reset_postdata();
+	?>
+	<div class="clear"></div>
+
+</div>
+
+<div class="sales_block background_none">
+	<ul class="ul_cat">
+		<li><img src="<?php bloginfo('template_directory'); ?>/images/cake.png"><a href="">Сладости и кофе<span>&#9660;</span></a>
+			<?php woocommerce_subcats_from_parentcat_by_ID(27); ?>
+		</li>
+	</ul>
+	<div class="clear"></div>
+	<?php
+	$args = array(
+		'post_type' => 'product',
+		'product_cat' => 'sweets',
+		'posts_per_page' => 4
+	);
+
+	$cont_num =0;
+	$loop = new WP_Query( $args );
+	if ( $loop->have_posts() ) {
+		while ( $loop->have_posts() ) : $loop->the_post();
+			++$cont_num;
+			?>
+			<div class="product_item">
+				<div class="border_right <?php if($cont_num==4) echo 'border_none'; ?>">
+					<a href="<?php the_permalink() ?>" class="img_product">
+						<?php echo woocommerce_get_product_thumbnail(); ?>
+					</a>
+					<a href="<?php the_permalink() ?>" title="Ссылка на: <?php the_title_attribute(); ?>" class="title_profuct"><?php the_title(); ?></a>
+					<div class="new_price"><?php echo $product->price; ?> сум</div>
+
+					<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
+					<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+					<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+
+					<?php
+					if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
+						$html = '<form  class="cart" method="post" product_id="'. $product->get_id() .  '" enctype="multipart/form-data">';
+						$html .= '<div class="num_select">'.woocommerce_quantity_input( array(), $product, false ).'</div>';
+						$html .= '<div class="but_add"><button type="submit">добавить</button></div>';
+						$html .= '</form>';
+						echo $html;
+					} elseif ( $product->is_type( 'variable' ) ) {
+					?>
+						<div class="but_add"><a href="<?php the_permalink() ?>">выбрать</a></div>
+					<?php } ?>
+					<div class="clear"></div>
+				</div>
+			</div>
+		<?php endwhile;
+	} else {
+		echo __( 'No products found' );
+	}
+	wp_reset_postdata();
+	?>
+	<div class="clear"></div>
+
+</div>
+
+<div class="sales_block background_none">
+	<ul class="ul_cat">
+		<li><img src="<?php bloginfo('template_directory'); ?>/images/market.png"><a href="">Маркет<span>&#9660;</span></a>
+			<?php woocommerce_subcats_from_parentcat_by_ID(28); ?>
+		</li>
+	</ul>
+	<div class="clear"></div>
+	<?php
+	$args = array(
+		'post_type' => 'product',
+		'product_cat' => 'shop',
 		'posts_per_page' => 4
 	);
 
