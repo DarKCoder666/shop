9a47eefdc9fb9ac0678727a958380a6d54e68670
DarKCoder666 <umid010699@mail.ru>
2017-12-02 12:17:05 +0500

    Написал виджет фильтрации.

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -20,7 +20,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-get_header( 'shop' ); ?>
+
+get_header( 'shop' ); 
+
+// В данном блоке получаем данные с get запроса, преобразуем в валидный для запроса на товары вид, и записываем в переменную $filter_params
+$filter_params = array();
+
+foreach ($_GET as $filter_name => $filter_values) {
+	if( stristr( $filter_name, 'filter_' ) ) {
+		$filter_params[ substr( $filter_name, 7 ) ] = explode(  ',', $filter_values );
+	}
+}
+////////////////////////////////////////////////////////////////////////
+?>
 
 <!--Главные категории-->
 <div class="wrap_block">
@@ -105,82 +117,9 @@ get_header( 'shop' ); ?>
 
     </header>
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php
-				/**
-				 * woocommerce_before_shop_loop hook.
-				 *
-				 * @hooked wc_print_notices - 10
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
-				do_action( 'woocommerce_before_shop_loop' );
-			?>
-
-			<?php woocommerce_product_loop_start(); ?>
-
-				<?php woocommerce_product_subcategories(); ?>
-
-				<?php while ( have_posts() ) : the_post(); global $product; ?>
-
-				<div class="product_item">
-					<div class="border_right <?php if($cont_num==4) echo 'border_none'; ?>">
-						<a href="<?php the_permalink() ?>" class="img_product">
-							<?php echo woocommerce_get_product_thumbnail(); ?>
-						</a>
-						<div class="item_text_block">
-							<a href="<?php the_permalink() ?>" title="Ссылка на: <?php the_title_attribute(); ?>" class="title_profuct"><?php the_title(); ?></a>
-							<div class="new_price"><?php echo $product->get_price(); ?> сум</div>
-
-							<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
-							<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
-							<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
-						</div>
-						<div class="right_but_add">
-							<?php
-							if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
-								$html = '<form action="' . esc_url( $product->add_to_cart_url() ) . '" class="cart" method="post" enctype="multipart/form-data">';
-								$html .= '<div class="num_select">'.woocommerce_quantity_input( array(), $product, false ).'</div>';
-								$html .= '<div class="but_add"><button type="submit">добавить</button></div>';
-								$html .= '</form>';
-								echo $html;
-							} elseif ( $product->is_type( 'variable' ) ) {
-								?>
-								<div class="but_add"><a href="<?php the_permalink() ?>">выбрать</a></div>
-							<?php } ?>
-						</div>
-						<div class="clear"></div>
-					</div>
-				</div>
-
-
-
-				<?php endwhile; // end of the loop. ?>
-
-			<?php woocommerce_product_loop_end(); ?>
-
-			<?php
-				/**
-				 * woocommerce_after_shop_loop hook.
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
-				do_action( 'woocommerce_after_shop_loop' );
-			?>
-
-		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-			<?php
-				/**
-				 * woocommerce_no_products_found hook.
-				 *
-				 * @hooked wc_no_products_found - 10
-				 */
-				do_action( 'woocommerce_no_products_found' );
-			?>
-
-		<?php endif; ?>
+	<!-- Самописаня функция, для отображения товаров на странице -->
+	<!-- Описана в файле get_filtred_products.php -->
+	<?php get_filtred_products( $filter_params, get_queried_object_id() ); ?>
 
 	<?php
 		/**
